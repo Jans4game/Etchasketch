3e64409d2a69a553c49fb7d4eecb13c966a2e517
Jans4game <mohammedjansheer@gmail.com>
2023-05-27 14:57:11 +0530

    index.html

---
+++ javascript.js
@@ -0,0 +1,159 @@
+//First row start
+
+const firstRow = document.querySelector('#rowone');
+
+const firstBox = document.createElement('div');
+firstBox.classList.add('firstBox');
+
+const secondBox = document.createElement('div');
+secondBox.classList.add('secondBox');
+
+const thirdBox = document.createElement('div');
+thirdBox.classList.add('thirdBox');
+
+const fourthBox = document.createElement('div');
+fourthBox.classList.add('fourthBox');
+
+firstRow.appendChild(firstBox);
+firstRow.appendChild(secondBox);
+firstRow.appendChild(thirdBox);
+firstRow.appendChild(fourthBox);
+
+//First row end
+
+//second row start
+
+const secondRow = document.querySelector('#rowtwo');
+
+const fifthBox = document.createElement('div');
+fifthBox.classList.add('fifthBox');
+
+const sixthBox = document.createElement('div');
+sixthBox.classList.add('sixthBox');
+
+const seventhBox = document.createElement('div');
+seventhBox.classList.add('seventhBox');
+
+const eighthBox = document.createElement('div');
+eighthBox.classList.add('eighthBox');
+
+secondRow.appendChild(fifthBox);
+secondRow.appendChild(sixthBox);
+secondRow.appendChild(seventhBox);
+secondRow.appendChild(eighthBox);
+
+//second row end
+
+//third row start
+
+const thirdRow = document.querySelector('#rowthree');
+
+const ninthBox = document.createElement('div');
+ninthBox.classList.add('ninthBox');
+
+const tenthBox = document.createElement('div');
+tenthBox.classList.add('tenthBox');
+
+const eleventhBox = document.createElement('div');
+eleventhBox.classList.add('eleventhBox');
+
+const twelfthBox = document.createElement('div');
+twelfthBox.classList.add('twelfthBox');
+
+thirdRow.appendChild(ninthBox);
+thirdRow.appendChild(tenthBox);
+thirdRow.appendChild(eleventhBox);
+thirdRow.appendChild(twelfthBox);
+
+//third row end
+
+//fourth row start
+
+const fourthRow = document.querySelector('#rowfour');
+
+const thirteenthBox = document.createElement('div');
+thirteenthBox.classList.add('thirteenthBox');
+
+const fourteenthBox = document.createElement('div');
+fourteenthBox.classList.add('fourteenthBox');
+
+const fifteenthBox = document.createElement('div');
+fifteenthBox.classList.add('fifteenthBox');
+
+const sixteenthBox = document.createElement('div');
+sixteenthBox.classList.add('sixteenthBox');
+
+fourthRow.appendChild(thirteenthBox);
+fourthRow.appendChild(fourteenthBox);
+fourthRow.appendChild(fifteenthBox);
+fourthRow.appendChild(sixteenthBox);
+
+//fourth row end
+
+
+
+
+document.querySelector('.firstBox').addEventListener("mouseover", function() {
+    document.querySelector(".firstBox").style.background='rgb(0,0,255)'
+}
+);
+document.querySelector('.secondBox').addEventListener("mouseover", function() {
+    document.querySelector(".secondBox").style.background='rgb(0,0,255)'
+}
+);
+document.querySelector('.thirdBox').addEventListener("mouseover", function() {
+    document.querySelector(".thirdBox").style.background='rgb(0,0,255)'
+}
+);
+document.querySelector('.fourthBox').addEventListener("mouseover", function() {
+    document.querySelector(".fourthBox").style.background='rgb(0,0,255)'
+}
+);
+document.querySelector('.fifthBox').addEventListener("mouseover", function() {
+    document.querySelector(".fifthBox").style.background='rgb(0,0,255)'
+}
+);
+document.querySelector('.sixthBox').addEventListener("mouseover", function() {
+    document.querySelector(".sixthBox").style.background='rgb(0,0,255)'
+}
+);
+document.querySelector('.seventhBox').addEventListener("mouseover", function() {
+    document.querySelector(".seventhBox").style.background='rgb(0,0,255)'
+}
+);
+document.querySelector('.eighthBox').addEventListener("mouseover", function() {
+    document.querySelector(".eighthBox").style.background='rgb(0,0,255)'
+}
+);
+document.querySelector('.ninthBox').addEventListener("mouseover", function() {
+    document.querySelector(".ninthBox").style.background='rgb(0,0,255)'
+}
+);
+document.querySelector('.tenthBox').addEventListener("mouseover", function() {
+    document.querySelector(".tenthBox").style.background='rgb(0,0,255)'
+}
+);
+document.querySelector('.eleventhBox').addEventListener("mouseover", function() {
+    document.querySelector(".eleventhBox").style.background='rgb(0,0,255)'
+}
+);
+document.querySelector('.twelfthBox').addEventListener("mouseover", function() {
+    document.querySelector(".twelfthBox").style.background='rgb(0,0,255)'
+}
+);
+document.querySelector('.thirteenthBox').addEventListener("mouseover", function() {
+    document.querySelector(".thirteenthBox").style.background='rgb(0,0,255)'
+}
+);
+document.querySelector('.fourteenthBox').addEventListener("mouseover", function() {
+    document.querySelector(".fourteenthBox").style.background='rgb(0,0,255)'
+}
+);
+document.querySelector('.fifteenthBox').addEventListener("mouseover", function() {
+    document.querySelector(".fifteenthBox").style.background='rgb(0,0,255)'
+}
+);
+document.querySelector('.sixteenthBox').addEventListener("mouseover", function() {
+    document.querySelector(".sixteenthBox").style.background='rgb(0,0,255)'
+}
+);
